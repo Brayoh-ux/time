912fe4113b410d09be79f258d6b2badf62712b1e
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-01-04 17:23:11 +0300

    initial

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
 function realtimeClock() {
     var rtClock = new Date();
+    // var year = rtClock.getFullYear();
+    // var month = rtClock.getMonth();
+    // var day = rtClock.getDay();
+    // var hours = rtClock.getHours();
+    // var minutes = rtClock.getMinutes();
+    // var seconds = rtClock.getSeconds();
+
+    // var e = " : " + month + " : " + day;
+
+    // // add am & pm
+    // var amPm = (hours < 12 ) ? "AM" : "PM";
+
+    // // convert hours to 12-hour format
+    // hours = (hours > 12) ? hours - 12 : hours;
+
+    // // pad hours, mins and sec with leading zeros
+    // hours = ("0" + hours).slice(-2);
+    // minutes = ("0" + minutes).slice(-2);
+    // seconds = ("0" + seconds).slice(-2); 
     
-    var hours = rtClock.getHours();
-    var minutes = rtClock.getMinutes();
-    var seconds = rtClock.getSeconds();
-
-    // add am & pm
-    var amPm = (hours < 12 ) ? "AM" : "PM";
-
-    // convert hours to 12-hour format
-    hours = (hours > 12) ? hours - 12 : hours;
-
-    // pad hours, mins and sec with leading zeros
-    hours = ("0" + hours).slice(-2);
-    minutes = ("0" + minutes).slice(-2);
-    seconds = ("0" + seconds).slice(-2); 
 
 
 // display clock
-    document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+    document.getElementById('clock').innerHTML = rtClock;
     var t = setTimeout(realtimeClock, 500);
 
+    // document.getElementById('day').innerHTML = year + e.UTCString();
 }
